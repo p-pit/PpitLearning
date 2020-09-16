3febc35ca7d64fb1bfe46fd71dfa6271e48d98f1
bruno <bruno@MacBook-Pro-de-Bruno.local>
2020-09-16 14:14:11 +0200

    teacher/init

--- src/PpitLearning/Controller/TeacherController.php
+++ src/PpitLearning/Controller/TeacherController.php
@@ -646,11 +646,11 @@ class TeacherController extends AbstractActionController
 				$vcard->update(null);
 				print_r(['id' => $teacher->id, 'email' => $teacher->email, 'place_caption' => $teacher->place_caption, 'contact_1_id' => $teacher->contact_1_id]);
 			}
-/*			if ($vcard->applications && $vcard->roles == ['teacher' => 'teacher']) {
+			if ($vcard->applications && $vcard->roles == ['teacher' => 'teacher']) {
 				$vcard->applications = [];
 				$vcard->update(null);
 				print_r(['id' => $teacher->id, 'email' => $teacher->email, 'place_caption' => $teacher->place_caption, 'contact_1_id' => $teacher->contact_1_id, 'roles' => $vcard->roles, 'applications' => $vcard->applications]);
-			}*/
+			}
 		}
 		
 		// Get the vcard with role 'teacher'
