cd458c1d020fab91dd81222f3a362b5bbaeaff7d
blartillot <lartillotb@users.noreply.github.com>
2021-06-28 12:48:24 +0200

    Bug fix

--- src/PpitLearning/Controller/TeacherController.php
+++ src/PpitLearning/Controller/TeacherController.php
@@ -485,7 +485,7 @@ class TeacherController extends AbstractActionController
 		// user_story - student_evaluation_teachers: Les enseignants pouvant être selectionnés dans le formulaire sont tous les enseignants ayant un statut "actif"
 		$teachers = [];
 		if ($context->hasRole('manager') && !$context->hasRole('teacher')) {
-			$cursor = Account::getList('teacher', ['status' => 'active,committed,contrat_envoye,reconnect_with'], '+name', null);
+			$cursor = Account::getList('teacher', ['status' => 'active,committed,contrat_envoye,reconnect_with'], '+name', null, null);
 			foreach ($cursor as $teacher_id => $teacher) {
 				$teachers[$teacher->contact_1_id] = $teacher->properties;
 				$competences = $teachers[$teacher->contact_1_id]['property_3'];
